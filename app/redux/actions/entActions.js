76a80e8142a6c374b07efc4e4a176739f042ba9b
bqm3 <bqminh30@gmail.com>
2024-03-25 17:35:23 +0700

    update config and redux

--- app/redux/actions/entActions.js
+++ app/redux/actions/entActions.js
@@ -209,3 +209,59 @@ export const ent_chucvu_get = () => {
     }
   };
 };
+
+export const ent_checklist_get = () => {
+  return async (dispatch) => {
+    try {
+      let token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IklEX1VzZXIiOjMsIlVzZXJOYW1lIjoiQWRtaW4iLCJQZXJtaXNzaW9uIjoxLCJJRF9EdWFuIjoxLCJQYXNzd29yZCI6IiQyYiQxMCRXbEZYdjgyd1hsTzZyaHNmS245dk1lNkgvdUU0ZXA0R3FiWGhsTjhCeXliVjlRSDY5QTNJNiIsIklEX0tob2lDViI6MSwiRW1haWxzIjoicGhvbmdzb2hvYUBwbWN3ZWIudm4iLCJpc0RlbGV0ZSI6MCwiZW50X2R1YW4iOnsiRHVhbiI6IkThu7Egw6FuIFZOUFQifSwiZW50X2NodWN2dSI6eyJDaHVjdnUiOiJHacOhbSDEkeG7kWMgZOG7sSDDoW4ifX0sImlhdCI6MTcxMDkwMDUwMiwiZXhwIjoxNzExNTA1MzAyfQ.3E9JQc3tYtieUw93-1Nx5qzkgiQFa4PBXZYfQ0PFOss";
+      if (token !== null) {
+        const response = await axios.get(BASE_URL + "/ent_checklist", {
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
+        const data = response.data.data;
+        dispatch({
+          type: type.SET_ENT_CHECKLIST_SUCCESS,
+          payload: {
+            ent_checklist: data,
+          },
+        });
+      } else {
+        console.error("initialized error");
+      }
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+};
+
+export const ent_tang_get = () => {
+  return async (dispatch) => {
+    try {
+      let token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IklEX1VzZXIiOjMsIlVzZXJOYW1lIjoiQWRtaW4iLCJQZXJtaXNzaW9uIjoxLCJJRF9EdWFuIjoxLCJQYXNzd29yZCI6IiQyYiQxMCRXbEZYdjgyd1hsTzZyaHNmS245dk1lNkgvdUU0ZXA0R3FiWGhsTjhCeXliVjlRSDY5QTNJNiIsIklEX0tob2lDViI6MSwiRW1haWxzIjoicGhvbmdzb2hvYUBwbWN3ZWIudm4iLCJpc0RlbGV0ZSI6MCwiZW50X2R1YW4iOnsiRHVhbiI6IkThu7Egw6FuIFZOUFQifSwiZW50X2NodWN2dSI6eyJDaHVjdnUiOiJHacOhbSDEkeG7kWMgZOG7sSDDoW4ifX0sImlhdCI6MTcxMDkwMDUwMiwiZXhwIjoxNzExNTA1MzAyfQ.3E9JQc3tYtieUw93-1Nx5qzkgiQFa4PBXZYfQ0PFOss";
+      if (token !== null) {
+        const response = await axios.get(BASE_URL + "/ent_tang", {
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
+        const data = response.data.data;
+        dispatch({
+          type: type.SET_ENT_TANG_SUCCESS,
+          payload: {
+            ent_tang: data,
+          },
+        });
+      } else {
+        console.error("initialized error");
+      }
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+};
