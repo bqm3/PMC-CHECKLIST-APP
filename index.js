9dfae2dd7dd9b39bdfa85ceba810751f7812ec9b
bqm3 <bqminh30@gmail.com>
2024-03-25 17:34:07 +0700

    update folder android

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { registerRootComponent } from 'expo';
+import {Text, TextInput} from 'react-native';
 
 import App from './App';
+if (Text.defaultProps == null) {
+    Text.defaultProps = {};
+    Text.defaultProps.allowFontScaling = false;
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
