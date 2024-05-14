82ecdd31156be53c43c51eb9820b49911f1d334f
bqm3 <bqminh30@gmail.com>
2024-05-14 10:37:49 +0700

    Update version and versionCode for Android

---
+++ app.config.js
@@ -0,0 +1,11 @@
+export default {
+    expo: {
+      name: "PMC-CHECKLIST-APP",
+      slug: "PMC-CHECKLIST-APP",
+      version: "1.0.1", // Update this to the new version
+      android: {
+        versionCode: 2 // Increment this number
+      }
+    }
+  };
+  
