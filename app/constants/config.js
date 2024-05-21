713105197b43f0522a9bfcbffe055965dcfa22a5
bqm3 <bqminh30@gmail.com>
2024-05-21 10:41:02 +0700

    default config

--- app/constants/config.js
+++ app/constants/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL="https://checklist.pmcweb.vn/be/api" 
-export const BASE_URL="https://6443-113-164-4-5.ngrok-free.app/api"
+export const BASE_URL="https://checklist.pmcweb.vn/be/api" 
+// export const BASE_URL="https://6443-113-164-4-5.ngrok-free.app/api"
