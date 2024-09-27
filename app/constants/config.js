178025a9946a57b001977ca91614ca786b4263aa
bqm3 <bqminh30@gmail.com>
2024-09-27 15:16:47 +0700

    final update v2

--- app/constants/config.js
+++ app/constants/config.js
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://checklist.pmcweb.vn/be/api/v2";
-// export const BASE_URL="https://8488-2001-ee0-2f00-96-509-c9d0-7587-7550.ngrok-free.app/api"
+// export const BASE_URL="https://c80c-2001-ee0-2f00-96-11b7-31e8-5d56-41f9.ngrok-free.app/api/v2"
